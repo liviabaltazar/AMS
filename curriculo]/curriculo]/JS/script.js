271fd23742dc84bef1f2f3b4a8e876d978fa7528
liviabaltazar <liviabaltazar2810@gmail.com>
2026-04-30 13:39:38 -0300

    Add files via upload

---
+++ curriculo]/curriculo]/JS/script.js
@@ -0,0 +1,17 @@
+const botao = document.getElementById("Tema")
+
+//caregar preferência
+if (localStorage.getItem("Tema") == "dark"){
+    document.body.classList.add("dark")
+}
+
+// alternar tema
+botao.addEventListener("click", ()=> {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")){
+        localStorage.setItem("tema", "dark");
+    } else {
+        localStorage.setItem("tema", "light")
+    }
+});
